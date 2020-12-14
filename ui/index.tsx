45f3efad857f024d9ff510a1282cdf28efeb7fe0
tairinjane <tairinjane@gmail.com>
2020-12-14 16:05:10 +0700

    feat: move container to page layout

--- ui/index.tsx
+++ ui/index.tsx
@@ -7,15 +7,18 @@ import { Route, Switch } from 'react-router';
 import { BooksPage } from './components/books/books-page';
 import { NavBar } from './components/nav-bar/nav-bar';
 import { MainContent } from './components/containers/main-content';
+import { Container } from '@material-ui/core';
 
 const App = () => (
   <BrowserRouter basename="/">
     <NavBar />
     <MainContent>
-      <Switch>
-        <Route exact path="/" component={BooksPage} />
-        <Route render={() => <div>No such page</div>} />
-      </Switch>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={BooksPage} />
+          <Route render={() => <div>No such page</div>} />
+        </Switch>
+      </Container>
     </MainContent>
   </BrowserRouter>
 );
