fa84c49f695fbe5be7971babd371524ac4886612
tairinjane <tairinjane@gmail.com>
2020-12-15 21:06:15 +0700

    chore: extract books search controls

--- ui/components/books/books-page.tsx
+++ ui/components/books/books-page.tsx
@@ -1,45 +1,16 @@
-import React, { useCallback, useState } from 'react';
-import { Button, InputGroup } from '@blueprintjs/core';
-import { Grid } from '@material-ui/core';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { TBook, TStore } from '../../store/store';
-import { BooksThunks } from '../../actions/books.thunks';
 import { BooksTable } from './books-table';
+import { BooksSearchInputs } from './books-search-inputs';
 
 export const BooksPage = () => {
-  const dispatch = useDispatch();
   const books = useSelector<TStore, TBook[]>(store => store.search.books);
-  const [title, setTitle] = useState('');
-
-  const handleSearch = useCallback(() => {
-    dispatch(BooksThunks.findBooks(title));
-  }, [title]);
 
   return (
     <>
       <h1 className="bp3-heading offset-bottom-24">Books</h1>
-      <Grid container spacing={2} alignItems={'flex-end'} wrap={'wrap'}>
-        <Grid item xs={5}>
-          <span className="input-title">Title</span>
-          <InputGroup
-            placeholder={'Title'}
-            className="offset-top-8"
-            value={title}
-            onChange={(e: React.FormEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)}
-          />
-        </Grid>
-        <Grid item xs>
-          <span className="input-title">Author Last Name</span>
-          <InputGroup placeholder={'Author Last Name'} className="offset-top-8" />
-        </Grid>
-        <Grid item xs>
-          <span className="input-title">Author First Name</span>
-          <InputGroup placeholder={'Author First Name'} className="offset-top-8" />
-        </Grid>
-        <Grid item>
-          <Button icon={'search'} intent={'primary'} onClick={handleSearch} />
-        </Grid>
-      </Grid>
+      <BooksSearchInputs />
       {!!books?.length ? <BooksTable books={books} /> : <div className="offset-top-24 text-center">No results</div>}
     </>
   );
---
+++ ui/components/books/books-search-inputs.tsx
@@ -0,0 +1,51 @@
+import { Grid } from '@material-ui/core';
+import { Button, InputGroup } from '@blueprintjs/core';
+import React, { useCallback, useState } from 'react';
+import { BooksThunks } from '../../actions/books.thunks';
+import { useDispatch } from 'react-redux';
+
+export const BooksSearchInputs = () => {
+  const dispatch = useDispatch();
+  const [authorFirstName, setAuthorFirstName] = useState('');
+  const [authorLastName, setAuthorLastName] = useState('');
+  const [title, setTitle] = useState('');
+
+  const handleSearch = useCallback(() => {
+    dispatch(BooksThunks.findBooks(title, authorFirstName, authorLastName));
+  }, [title, authorFirstName, authorLastName]);
+
+  return (
+    <Grid container spacing={2} alignItems={'flex-end'} wrap={'wrap'}>
+      <Grid item xs={5}>
+        <span className="input-title">Title</span>
+        <InputGroup
+          placeholder={'Title'}
+          className="offset-top-8"
+          value={title}
+          onChange={(e: React.FormEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)}
+        />
+      </Grid>
+      <Grid item xs>
+        <span className="input-title">Author Last Name</span>
+        <InputGroup
+          placeholder={'Author Last Name'}
+          className="offset-top-8"
+          value={authorLastName}
+          onChange={(e: React.FormEvent<HTMLInputElement>) => setAuthorLastName(e.currentTarget.value)}
+        />
+      </Grid>
+      <Grid item xs>
+        <span className="input-title">Author First Name</span>
+        <InputGroup
+          placeholder={'Author First Name'}
+          className="offset-top-8"
+          value={authorFirstName}
+          onChange={(e: React.FormEvent<HTMLInputElement>) => setAuthorFirstName(e.currentTarget.value)}
+        />
+      </Grid>
+      <Grid item>
+        <Button icon={'search'} intent={'primary'} onClick={handleSearch} />
+      </Grid>
+    </Grid>
+  );
+};
