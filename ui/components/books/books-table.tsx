f6ce4b595abb7bf93d1dfbc1cdcdb2ad558db8b3
tairinjane <tairinjane@gmail.com>
2020-12-16 16:02:37 +0700

    feat: row onClick for books table

--- ui/components/books/books-table.tsx
+++ ui/components/books/books-table.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { TBook } from '../../store/store';
-import { Cell, Column, Table } from '@blueprintjs/table';
+import { Cell, Column, SelectionModes, Table } from '@blueprintjs/table';
+import { IFocusedCellCoordinates } from '@blueprintjs/table/lib/esm/common/cell';
 
 type Props = {
   books?: TBook[];
+  onRowClick?: (rowIndex: number) => void;
 };
 
-export const BooksTable = ({ books }: Props) => {
+export const BooksTable = ({ books, onRowClick }: Props) => {
   const titleCellRenderer = (rowIndex: number) => <Cell>{books[rowIndex]?.title}</Cell>;
   const authorCellRenderer = (rowIndex: number) => {
     const authors = books[rowIndex]?.authors.map(
@@ -17,8 +19,20 @@ export const BooksTable = ({ books }: Props) => {
   const amountRenderer = (rowIndex: number) => <Cell>{books[rowIndex]?.amount}</Cell>;
   const availableRenderer = (rowIndex: number) => <Cell>{books[rowIndex]?.available}</Cell>;
 
+  const onFocus = useCallback((focusedCell: IFocusedCellCoordinates) => {
+    console.log(JSON.stringify(focusedCell));
+    onRowClick(focusedCell.row);
+  }, []);
+
   return (
-    <Table numRows={books?.length} defaultRowHeight={35} enableRowResizing={false} className="offset-top-24">
+    <Table
+      numRows={books?.length}
+      enableRowResizing={false}
+      className="offset-top-24"
+      enableFocusedCell={!!onRowClick}
+      onFocusedCell={!!onRowClick ? onFocus : null}
+      selectionModes={SelectionModes.ROWS_AND_CELLS}
+    >
       <Column name={'Title'} cellRenderer={titleCellRenderer} />
       <Column name={'Author'} cellRenderer={authorCellRenderer} />
       <Column name={'Amount'} cellRenderer={amountRenderer} />
