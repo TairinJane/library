bcc0f4a05fa4ed9be229d5a466c5d840192d14b2
tairinjane <tairinjane@gmail.com>
2020-12-18 13:37:53 +0700

    feat: reorganize navbar

--- ui/components/nav-bar/nav-bar.tsx
+++ ui/components/nav-bar/nav-bar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Drawer, List } from '@material-ui/core';
+import { Divider, Drawer, List } from '@material-ui/core';
 import { ListItemLink } from './list-item-link';
 
 export const NavBar = () => {
@@ -7,11 +7,16 @@ export const NavBar = () => {
     <Drawer variant={'permanent'} anchor={'left'} className="nav-bar" classes={{ paper: 'nav-bar' }}>
       <List>
         <ListItemLink text={'Home'} to={'/'} />
+        <Divider />
         <ListItemLink text={'Books Search'} to={'/search/books'} />
-        <ListItemLink text={'Readers Search'} to={'/search/readers'} />
-        <ListItemLink text={'Lend a Book'} to={'/lend'} />
+        <ListItemLink text={'Lend Book'} to={'/lend'} />
         <ListItemLink text={'Due Books'} to={'/due'} />
+        <Divider />
+        <ListItemLink text={'Readers Search'} to={'/search/readers'} />
         <ListItemLink text={'Add Reader'} to={'/reader/new'} />
+        <Divider />
+        <ListItemLink text={'Purchases'} to={'/search/purchases'} />
+        <ListItemLink text={'New Purchase'} to={'/purchase/new'} />
       </List>
     </Drawer>
   );
