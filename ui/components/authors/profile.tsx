d244d8e1dba894a5db8ab9ec511a7cdfa4932a86
tairinjane <tairinjane@gmail.com>
2021-01-16 20:43:24 +0700

    feat(author-profile): redirect to book profile on table click

--- ui/components/authors/profile.tsx
+++ ui/components/authors/profile.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps, useHistory } from 'react-router';
 import { Grid } from '@material-ui/core';
 import { TAuthorProfile, TStore } from '../../store/store';
 import { AuthorsThunks } from '../../actions/authors/authors.thunks';
 import { personFullName } from '../../utils/title.utils';
 import { AuthorBooksTable } from './author-books-table';
+import { BookActions } from '../../actions/books/books.actions';
 
 export const AuthorProfile = ({ match }: RouteComponentProps<{ id?: string }>) => {
+  const history = useHistory();
   const dispatch = useDispatch();
   const authorId = +match.params.id;
   const { author, books } = useSelector<TStore, TAuthorProfile>(store => store.authors.profiles[authorId]) || {};
@@ -19,6 +21,15 @@ export const AuthorProfile = ({ match }: RouteComponentProps<{ id?: string }>) =
   useEffect(() => {
     if (!books) dispatch(AuthorsThunks.getAuthorBooks(authorId));
   }, [books, authorId]);
+
+  const onBookSelect = useCallback(
+    (rowIndex: number) => {
+      const book = books[rowIndex];
+      dispatch(BookActions.saveBookInfo(book));
+      history.push(`/books/${book.id}`);
+    },
+    [books],
+  );
 
   return !!author ? (
     <>
@@ -32,7 +43,7 @@ export const AuthorProfile = ({ match }: RouteComponentProps<{ id?: string }>) =
         </Grid>
       </Grid>
       <h2 className="bp3-heading offset-bottom-24 offset-top-24">Books</h2>
-      {!!books?.length ? <AuthorBooksTable books={books} /> : 'No books found'}
+      {!!books?.length ? <AuthorBooksTable books={books} onRowClick={onBookSelect} /> : 'No books found'}
     </>
   ) : (
     <h1 className="bp3-heading offset-bottom-24">Author is loading...</h1>
